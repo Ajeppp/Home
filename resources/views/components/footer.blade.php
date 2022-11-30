622c5f8e0140721ea9cd61c09c63df27863fae2e
geraldimanuelstudent <94883672+geraldimanuelstudent@users.noreply.github.com>
2022-12-01 01:21:50 +0700

    commit for footer, ady responsive, no socmed logos

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,42 @@
-<div>
-    <h1>ini footer</h1>
+<div class="flex flex-col md:flex-row bg-[#F0F0F0] w-screen px-10 pt-16 pb-10 md:justify-between">
+    <div class="flex flex-col gap-6">
+        <div class="flex flex-row -mx-8 gap-5">
+            <img src="https://gpdi-hebron.com/website/images/banner/gpdi-hebron-gading-serpong-41181523042016.png" alt="" width="100px">
+            <img src="http://www.gpdi-hebron.com/website/images/media/7a015998c9ecd4adba19674917d15b673658eb91.png" alt="" width="100px">
+            <img src="https://gpdi-hebron.com/website/images/banner/right-banner-24221106122015.png" alt="" width="100px">
+        </div>
+        <p class="font-medium">Pasar Modern Paramount <br />
+            Jl. Boulevard Gading Serpong <br>
+            Ruko Thematic <br> 
+            Blok L39 & L50 <br>
+            Gading Serpong
+        </p>
+        <p class="font-bold text-sm -mt-10 md:mt-10 text-[#858585] invisible md:visible">Copyrights © 2022 GpDI Hebron, All Rights Reserved.</p> 
+        </div>
+        <div>
+            <div class="flex flex-col md:flex-row gap-7 md:gap-20 mr-7">
+                <div>
+                    <p class="font-bold">Service</p>
+                    <p class="font-medium text-[#0A0A0A]">Online Service</p>
+                    <p class="font-medium text-[#0A0A0A]">Onsite Service</p>
+                </div>
+                <div>
+                    <p class="font-bold">Get Involved</p>
+                    <p class="font-medium text-[#0A0A0A]">Christian Group</p>
+                </div>
+                <div>
+                    <p class="font-bold">Hebron</p>
+                    <p class="font-medium text-[#0A0A0A]">About</p>
+                    <p class="font-medium text-[#0A0A0A]">Contact</p>
+                    <p class="font-medium text-[#0A0A0A]">Carrers</p>
+                    <p class="font-medium text-[#0A0A0A]">FAQ</p>
+                    <p class="font-medium text-[#0A0A0A]">Resource</p>
+                </div>
+                <div>
+                    <p class="font-bold">Partners</p>
+                    <p class="font-medium text-[#0A0A0A]">Job Vacancy</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
