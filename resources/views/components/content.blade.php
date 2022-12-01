f01882f8c90e60d40e7caa4b4f44c5a8a708b29a
Jef <94884517+Ajeppp@users.noreply.github.com>
2022-12-01 15:02:58 +0700

    Create about us route and view

--- resources/views/components/content.blade.php
+++ resources/views/components/content.blade.php
@@ -1,7 +1,7 @@
-    <button>
-            <div class="flex bg-[#FFFFFF] hover:bg-[#434242] hover:text-[#FFFFFF] text-[#000000] w-[180px] h-[50px] items-center justify-center">
-                <a href="https://www.youtube.com/watch?v=JlBlOu1i_Jw&list=RDJlBlOu1i_Jw&start_radio=1" class="font-medium text-[18px]">
-                    Listen Now
-                </a>
-        </div>
-    </button>
+<button>
+    <div class="flex bg-[#FFFFFF] hover:bg-[#434242] hover:text-[#FFFFFF] text-[#000000] w-[180px] h-[50px] items-center justify-center">
+            <a href="https://www.youtube.com/watch?v=JlBlOu1i_Jw&list=RDJlBlOu1i_Jw&start_radio=1" class="font-medium text-[18px]">
+                Listen Now
+            </a>
+    </div>
+</button>
