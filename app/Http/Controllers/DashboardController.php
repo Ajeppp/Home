cbd52d5443f1577977cb1ff2f1a7f4285d13b07d
geraldimanuelstudent <94883672+geraldimanuelstudent@users.noreply.github.com>
2022-12-16 13:43:03 +0700

    salah route bang

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,7 +17,7 @@ class DashboardController extends Controller
 
     public function aboutus()
     {
-        return view('dashboard.aboutus', [
+        return view('dashboard.aboutUs', [
             'title' => 'About Us'
         ]);
     }
